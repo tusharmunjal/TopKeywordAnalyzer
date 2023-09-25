f4eebb7cbcf954dae8626bd33054067961c32712
tusharmunjal <103403431+tusharmunjal@users.noreply.github.com>
2023-09-25 09:11:19 +0530

    Add files via upload

---
+++ KeywordCount.java
@@ -0,0 +1,11 @@
+package tech.codingclub.utility.GeneralClasses;
+
+public class KeywordCount {
+    public String keyword;
+    public int count;
+    public KeywordCount(String keyword,int count)
+    {
+        this.keyword=keyword;
+        this.count=count;
+    }
+}
